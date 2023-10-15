e360c878e6297d0bc9da73543c4a1ea284c1f41c
Koko Hany <kokoh2370@gmail.com>
2023-10-15 20:20:08 +0300

    first commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -20,12 +20,7 @@ const login = () => {
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
         >
-          <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-          />
-          Flowbite
+        Medical Login  
         </a>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
